e77f8e62b1bdccc17bb4948f8aa7e6a78fc8ab1c
franco <99002814+FrancoHei@users.noreply.github.com>
2023-03-25 06:02:51 +0900

    25/03

--- GameAward2023/Assets/SourceCode/Camera/followCamera.cs
+++ GameAward2023/Assets/SourceCode/Camera/followCamera.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class followCamera : MonoBehaviour
+public class FollowCamera : MonoBehaviour
 {
     private GameObject target;
     private Vector3    targetCameraVector;
+    private Vector2    cameraOffset;
+    private Vector2    cameraMovement;
+
+    public float       cameraSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        target             = GameObject.Find("Player");
+        target = GameObject.Find("Player");
         targetCameraVector = transform.position - target.transform.position;
     }
 
@@ -20,6 +25,15 @@ public class followCamera : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = target.transform.position + targetCameraVector;
+        transform.position = target.transform.position + targetCameraVector + new Vector3(cameraOffset.x, cameraOffset.y, 0.0f);
+        cameraOffset += cameraMovement * cameraSpeed;
+    }
+
+    public void OnCameraMove(InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>();
+        if (value == Vector2.zero)
+            cameraOffset = Vector2.zero;
+        cameraMovement = value;
     }
 }
